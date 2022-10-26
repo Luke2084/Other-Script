d7dad7cd3c3bab499ce03bb8615a75021ef8037d
Algar <57739158+Algar233@users.noreply.github.com>
2022-10-26 19:05:11 +0800

    Update Sky.js

--- Sky.js
+++ Sky.js
@@ -6,7 +6,7 @@ const headers = {
 'Connection' : `keep-alive`,
 'Content-Type' : `application/json`,
 'Host' : `mgbsdk.matrix.netease.com`,
-'User-Agent' : `Sky-iOS-Gold/187758 CFNetwork/1378.1 Darwin/22.0.0`,
+'User-Agent' : `Sky-iOS-Gold/198588 CFNetwork/1402 Darwin/22.2.0`,
 'Accept-Language' : `zh-CN,zh-Hans;q=0.9`,
 'X-Client-Sign' : `7c4d3acd0bfe8444a8909930c70356ae3b5c44e0df5f4eeb93c70b7f5b2ebcdb`
 };
@@ -20,9 +20,6 @@ const myRequest = {
 };
 
 $task.fetch(myRequest).then(response => {
-    console.log(response.statusCode + "\n\n" + response.body);
-    $done();
-}, reason => {
-    console.log(reason.error);
+    console.log(response.body);
     $done();
 });
