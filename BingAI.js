f3d442c40b182e4fe336c431d2be25bcd08e3e2b
Hutao <57739158+hutao571@users.noreply.github.com>
2023-04-24 15:02:45 +0800

    更新 BingAI.js

--- BingAI.js
+++ BingAI.js
@@ -1,7 +1,6 @@
 /*
 脚本原创：hutao571
 */
-   const headers = $request.headers;
 
-   header-replace (\r\nUser-Agent:.+?)\w+\/[\d\.]+(\r\n) AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0
+   ^http://www.bing.com header-replace (\r\nUser-Agent:.+?)\w+\/[\d\.]+(\r\n) AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0
    $done({ headers });
