86eac991fdaaef3dddafe582678550635c56a643
Hutao <57739158+hutao571@users.noreply.github.com>
2023-04-24 14:56:58 +0800

    更新 BingAI.js

--- BingAI.js
+++ BingAI.js
@@ -1,10 +1,7 @@
 /*
-脚本原创：LiuYaChen
+脚本原创：hutao571
 */
    const headers = $request.headers;
 
-   delete headers["user-agent"];
-   
-
-   headers['User-Agent'] = `AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0`;
+   header-replace User-Agent AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0
    $done({ headers });
