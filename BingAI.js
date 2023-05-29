2939589cdfc70d87ebc0063c4c12bd5494a6fc7a
Hutao <57739158+hutao571@users.noreply.github.com>
2023-05-29 10:31:43 +0800

    更新 BingAI.js

--- BingAI.js
+++ BingAI.js
@@ -1,6 +1,4 @@
 /*
 脚本原创：hutao571
 */
-
-   ^https:\/\/www\.bing\.com\/(search|new) header-replace (\r\nUser-Agent:.+?)\w+\/[\d\.]+(\r\n) AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0
-   $done({ headers });
+^https:\/\/www\.bing\.com\/(search|new) header-replace-regex User-Agent Version\/[\d\.]+\s+\w+\/[\d\.] +\s+Safari\/[\d\.] Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41
