3bdc0a6c099eac4cc8a74d5626387127514665dd
Hutao <57739158+hutao571@users.noreply.github.com>
2023-05-29 11:48:07 +0800

    Delete BingAI.js

--- BingAI.js
+++
@@ -1,4 +0,0 @@
-/*
-脚本原创：hutao571
-*/
-^https:\/\/www\.bing\.com\/(search|new) header-replace-regex User-Agent Version\/[\d\.]+\s+\w+\/[\d\.] +\s+Safari\/[\d\.] Chrome/110.0.0.0 Safari/537.36 Edg/110.0.1587.41
